ea7c8eb8d6f2408b7b16dc2dbf28973f9fa16596
leevscode <leevscode@gmail.com>
2023-09-18 12:40:40 +0900

    json 파일 로드 완료

---
+++ hooks/useInfo.ts
@@ -0,0 +1,3 @@
+const useInfo = () => {};
+
+export default useInfo;
--- types/info.ts
+++ types/info.ts
@@ -2,3 +2,7 @@
 type Lat = number;
 type Lng = number;
 export type Cordinates = [Lat, Lng];
+// json 에서 불러올 데이터 타입 정의
+export type Info = {
+  Cordinates: Cordinates;
+};
