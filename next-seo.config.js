f7cfa400f489ad88da2827969a67969d546cb536
leevscode <leevscode@gmail.com>
2023-09-19 12:09:50 +0900

    SEO 코드 수정 완료

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,11 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    i18n: {
-      /** https://nextjs.org/docs/advanced-features/i18n-routing#getting-started */
-      locales: ['ko'],
-      defaultLocale: 'ko',
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  titleTemplate: '%s - 서비스 페이지',
+  openGraph: {
+    type: 'website',
+    site_name: '건강증진센터 위치보기 서비스',
+    images: [
+      { url: 'https://nextjs.org/static/blog/next-13/twitter-card.png' },
+    ],
+  },
+  additionalLinkTags: [
+    {
+      rel: 'shortcut icon',
+      href: '/favicon.ico',
     },
-  };
-  
-  module.exports = nextConfig;
+  ],
+};
