6f15b05eb6a6120d6981d3b5dc1f382cb030c991
leevscode <leevscode@gmail.com>
2023-09-20 10:56:27 +0900

    seo config 추가

--- next-seo.config.js
+++ next-seo.config.js
@@ -17,7 +17,7 @@ export default {
   additionalMetaTags: [
     {
       name: 'naver-site-verification',
-      content: '7c81e579336699b5fc33ab7f2f36771fec5416d7',
+      content: '8f3d988804ec6be5f11b37166e381ec686c66e17',
     },
   ],
 };
